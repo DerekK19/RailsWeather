198633966f5a476f020186c2cba25735e80aca41
Derek Knight (Mac) <Derek@GordonKnight.co.uk>
2013-09-05 11:30:45 +1200

    Greatly simplified Interface to Static module

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
 	def tableless_page(*args)
 		if args.size == 3
-			tableless_page_private(args[0], "", args[1], args[2])
+			Static::tableless_page(current_header, current_navbar, args[0], "", left_menu_items, args[1], args[2])
 		else
-			tableless_page_private(args[0], "/" + args[1], args[2], args[3])
+			Static::tableless_page(current_header, current_navbar, args[0], "/" + args[1], left_menu_items, args[2], args[3])
 		end
 	end
 
@@ -28,27 +28,5 @@ module ApplicationHelper
 			['Sensors', '/sensors'],
 		]
 	end
-
-	def tableless_page_private(right_text, right_link, error_message, yield_data)
-	url_root = 	'http://' + request.host_with_port + Rails::configuration.action_controller.relative_url_root
-	Static::header(right_text, url_root+right_link, current_header, current_navbar) +
-	'
-	  <div class="page-content">
-		<div class="left-menu">
-		  ' + Static::left_menu(url_root, left_menu_items) + '
-		</div>
-		<div class="right-content">
-		  <br/>
-		  <div class="error-content">
-			<p style="color: green">' + (error_message.nil? ? "" : error_message) + '</p>
-		  </div>
-		  <br/>
-		  <div class="page-data">
-			' + yield_data + '
-		  </div>
-		</div>
-	  </div>
-
-	  ' + Static::footer(url_root)
-	end
+	
 end
