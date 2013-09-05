244aaaee5803a90973a83317ebcf8894ecb8c182
Derek Knight (Mac) <Derek@GordonKnight.co.uk>
2013-09-05 18:13:01 +1200

    Added current values to welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
   def index
+  	@temperature = Temperature.last
+  	@humidity = Humidity.last
+  	@wind = Wind.last
+  	@rainfall = Rainfall.last
+  	@wind_direction = ['N','NNE','NE','ENE','E','ESE','SE','SSE','S','SSW','SW','WSW','W','WNW','NW','NNW'][@wind.direction]
+  	@weather_date = @temperature.toDttm.strftime("%H:%M %d %b")
   end
 end
